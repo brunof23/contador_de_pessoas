06aa10dee7086f2033c12f6a6b4bf732c68cc3d7
Programador <brunoferreira.tecnico@gmail.com>
2020-07-01 18:54:32 -0300

    commit text and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,57 @@
-import 'package:flutter/material.dart' ;
+import 'package:flutter/material.dart';
 
-void main (){
-  runApp (MaterialApp(
-    title: "Contador de Pessoas",
-    home: Container(color: Colors.white,)
-      
-  ));
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/restaurante.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode Entrar!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
+          )
+        ],
+      )));
 }
